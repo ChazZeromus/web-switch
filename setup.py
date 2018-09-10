166abf77e6e0d29c7da85dd2d81535e179ba3fca
chazzeromus <chaz.zeromus@gmail.com>
2018-09-09 21:11:24 -0400

    auto find packages in setup

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 name = 'webswitch'
-package = 'webswitch'
 description = 'Channel-style websocket server'
-version = '0.2'
+version = '0.0.1'
 depends=[
 	'websockets>=6.0',
 	'dataclasses>=0.6',
@@ -21,7 +20,7 @@ setup(
 	description=description,
 	author='Chaz Zeromus',
 	author_email='chaz.zeromus@gmail.com',
-	packages=[package],
+	packages=find_packages('.', exclude=['tests']),
 	install_requires=depends,
 	entry_points=entry_points,
 )
