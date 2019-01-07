65f201014d0059fab1eaaf27e5a055e64e1e1860
chazzeromus <chaz.zeromus@gmail.com>
2019-01-06 23:41:46 -0500

    upgrading version to 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 name = 'webswitch'
 description = 'Channel-style websocket server'
-version = '0.0.1'
+version = '0.0.2'
 depends=[
 	'websockets>=6.0',
 	'dataclasses>=0.6',
